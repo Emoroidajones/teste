de076d3e6ba76314505f8f5753d6bea9f539c8dd
Emoroidajones <filiyuta.trilha01.2023@gmail.com>
2024-05-07 08:14:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+
+    // let slideCount = $('#slider ul li').length;
+    // let slideWidth = $('#slider ul li').width();
+    // let slideHeight = $('#slider ul li').height();
+    // let sliderUlWidht =  -350 + slideWidth;
+
+    
+
+    // console.log(slideCount)
+    // console.log(slideWidth)
+    // console.log(slideHeight)
+    // console.log(sliderUlWidht)
+
+
+    // $('#slider').css( {width: slideWidth, height: slideHeight} );   
+    // $('#slider ul').css({width: sliderUlWidht, marginLeft: -slideWidth});
+
+    // $('#slider ul li:last-child').prependTo('slider ul');
+
+    // function moveLeft(){
+    //     $('#slider ul').animate({
+    //         left: + slideWidth 
+            
+    //     }, 200, function(){
+    //         $('#slider ul  li:last-child').prependTo('#slider ul');
+    //         $('#slider ul').css('left', '');
+
+    //     });
+    //     console.log(left)
+    // };
+
+    // function moveRight(){
+    //     $('#slider ul').animate({
+    //         left: - slideWidth  
+    //     }, 200, function(){
+    //         $('#slider ul li:first-child').appendTo('#slider ul');
+    //         $('#slider ul').css('left', '');
+    //     });
+    // };
+
+    // $('a.control-prev').on('click', function(){
+
+    //     moveLeft();
+    // })
+
+    // $('a.control-next').on('click', function(){
+
+    //     moveRight();
+    // })
+
+
+    // $('#checkbox').on('change', function()
+    //  { setInterval(function() { 
+        
+    //      moveLeft(); 
+    //     },3000);      
+    // })
+
